91a3486fab74fd677adf09965a9d82c5b02e0de1
arunyang2 <txblade07@gmail.com>
2024-10-09 14:36:39 +0900

    Update Color mark of Naver Cafe.js

--- Color mark of Naver Cafe.js
+++ Color mark of Naver Cafe.js
@@ -5,6 +5,9 @@
 // @description  방문페이지 읽음 유색표시(회색)
 // @match        https://cafe.naver.com/ArticleList.nhn*
 // @match        https://cafe.naver.com/ArticleRead.nhn*
+// @match        https://cafe.naver.com/MyCafeIntro.nhn*
+// @match        https://cafe.naver.com/ca-fe/cafes*
+// @icon         https://i.namu.wiki/i/jTrpSWv0M8WzqIN4yAlmdzJzwGnkOOlppmeBlpvGBTVUgFTFh2jB1cYe3FqqAs5nivbxxsXamrHBystBYJBbMUSexJyL5XEThTBRRH9Ko097HhpIUpFpnHgCOmdK6wm3AhIoJmLHazq0deITqR_ClA.webp
 // @grant        none
 // ==/UserScript==
 document.head.innerHTML+=`<style>a.article:visited,a.tit:visited{color:#ccc !important;}</style>`;
