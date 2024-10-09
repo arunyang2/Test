a55346ab5863976abc549876f3c0c16275bda823
arunyang2 <txblade07@gmail.com>
2024-10-09 15:37:41 +0900

    Update Color_mark_of_Naver_Cafe.user.js

--- Color_mark_of_Naver_Cafe.user.js
+++ Color_mark_of_Naver_Cafe.user.js
@@ -5,7 +5,7 @@
 // @description  방문페이지 읽음 유색표시(회색)
 // @match        https://cafe.naver.com/ArticleList.nhn*
 // @match        https://cafe.naver.com/ArticleRead.nhn*
-// @match        https://cafe.naver.com/MyCafeIntro.nhn*
+// @match        https://cafe.naver.com/ca-fe/cafes*
 // @icon         https://i.namu.wiki/i/jTrpSWv0M8WzqIN4yAlmdzJzwGnkOOlppmeBlpvGBTVUgFTFh2jB1cYe3FqqAs5nivbxxsXamrHBystBYJBbMUSexJyL5XEThTBRRH9Ko097HhpIUpFpnHgCOmdK6wm3AhIoJmLHazq0deITqR_ClA.webp
 // @run-at       document-start
 // @grant        none
