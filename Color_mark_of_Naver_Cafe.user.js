6f35a6f0f9870361470d1b66553e7aafb4c112a2
arunyang2 <txblade07@gmail.com>
2024-10-09 15:35:04 +0900

    Update Color_mark_of_Naver_Cafe.user.js

--- Color_mark_of_Naver_Cafe.user.js
+++ Color_mark_of_Naver_Cafe.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         네이버카페 방문페이지 읽음 유색표시(회색)
 // @author       알빠냥이
-// @version      2024-10-09.rev_4
+// @version      2024-10-09.rev_5
 // @description  방문페이지 읽음 유색표시(회색)
 // @match        https://cafe.naver.com/ArticleList.nhn*
 // @match        https://cafe.naver.com/ArticleRead.nhn*
+// @match        https://cafe.naver.com/MyCafeIntro.nhn*
 // @icon         https://i.namu.wiki/i/jTrpSWv0M8WzqIN4yAlmdzJzwGnkOOlppmeBlpvGBTVUgFTFh2jB1cYe3FqqAs5nivbxxsXamrHBystBYJBbMUSexJyL5XEThTBRRH9Ko097HhpIUpFpnHgCOmdK6wm3AhIoJmLHazq0deITqR_ClA.webp
 // @run-at       document-start
 // @grant        none
