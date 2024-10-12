597dd53cea6c03aafa94a10158af9f5335b7e11a
arunyang2 <txblade07@gmail.com>
2024-10-13 02:31:55 +0900

    Update Color_mark_of_StelLive_Cafe_Member.user.js

--- Color_mark_of_StelLive_Cafe_Member.user.js
+++ Color_mark_of_StelLive_Cafe_Member.user.js
@@ -1,17 +1,17 @@
 // ==UserScript==
-// @name         스텔라이브카페 멤버이름 유색표시
+// @name         스텔라이브 네이버카페 멤버이름 유색표시(말머리 부분)
 // @author       알빠냥이
 // @version      2024-10-13.rev1
-// @description  스텔라이브카페 멤버이름 유색표시
-// @match        https://cafe.naver.com/tteokbokk1*
-// @match        https://cafe.naver.com/ArticleList.nhn*
-// @match        https://cafe.naver.com/ArticleRead.nhn*
-// @match        https://cafe.naver.com/ca-fe/cafes*
+// @description  스텔라이브 네이버카페 멤버이름 유색표시(말머리 부분)
+// @match        https://*cafe.naver.com/ArticleList.nhn?search.clubid=29424353*
+// @match        https://*cafe.naver.com/ArticleRead.nhn*&clubid=29424353*
+// @match        https://cafe.naver.com/ca-fe/cafes/29424353*
 // @icon         https://i.namu.wiki/i/m4qQ4EBnF2iQuAmSA-or1PgDvbER6RsKJjgEuX9ZlfDV5RUXPKWe00Cdl-18THY3r0p4W-K7Jm2JeDNFpDhFki01MrgYJcirq1yu-L5Usu2AeFt0MiVXDo6HPqurRh_hKs2JkSxTL2ZwLx2rE4Gc8w.svg
 // @run-at       document-start
 // @grant        none
 // ==/UserScript==
 
-document.head.innerHTML+=`<style>a.article:visited,a.tit:visited{color:#d3d3d3 !important;}</style>
-<style>#upperArticleList.article-board.m-tcol-c{a.article:visited{color:#FFD2D2 !important;}}</style>
-<style>#upperArticleList.article-board.m-tcol-c{tr.board-notice.type_up{a.article:visited{color:#d3d3d3 !important;}}}</style>`;
+document.head.innerHTML+=`<style>span.head=히나{color:#e4002b !important;}</style>
+<style>span.head=타비{color:#70c5e8 !important;}</style>
+<style>span.head=마시로{color:#282828 !important;}</style>
+<style>span.head=리제{color:#971b2e !important;}</style>`;
