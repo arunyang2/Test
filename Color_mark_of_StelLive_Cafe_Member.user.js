7ffee3ae5e773e75beae1b515db8c577688f71f7
arunyang2 <txblade07@gmail.com>
2024-12-17 23:02:13 +0900

    Update Color_mark_of_StelLive_Cafe_Member.user.js

--- Color_mark_of_StelLive_Cafe_Member.user.js
+++ Color_mark_of_StelLive_Cafe_Member.user.js
@@ -6,7 +6,7 @@
 // @match        https://*cafe.naver.com/ArticleList.nhn?search.clubid=29424353*
 // @match        https://*cafe.naver.com/ArticleRead.nhn*&clubid=29424353*
 // @match        https://cafe.naver.com/ca-fe/cafes/29424353*
-// @icon         https://i.namu.wiki/i/m4qQ4EBnF2iQuAmSA-or1PgDvbER6RsKJjgEuX9ZlfDV5RUXPKWe00Cdl-18THY3r0p4W-K7Jm2JeDNFpDhFki01MrgYJcirq1yu-L5Usu2AeFt0MiVXDo6HPqurRh_hKs2JkSxTL2ZwLx2rE4Gc8w.svg
+// @icon         https://i.namu.wiki/i/Q_FCFrVYsgDw7uvNUxfq9sknhP_OVYHS228Cwbbjpj1POyNXo4J43yzeEnwoaD0ubmztVraNQonL0shv2rJLeAb4TCBIBr3xuiKkRiKchWd5R_wywdzNBKKgQzk80unFGMuhNz2gpzlMyM12komdQQ.webp
 // @run-at       document-start
 // @grant        none
 // ==/UserScript==
