fddf17a385399845e0fff14357394afdc5d6ef53
arunyang2 <txblade07@gmail.com>
2025-02-27 12:11:36 +0900

    Update Color_mark_of_StelLive_Naver_Cafe.user.js

--- Color_mark_of_StelLive_Naver_Cafe.user.js
+++ Color_mark_of_StelLive_Naver_Cafe.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         스텔라이브 네이버카페 방문페이지 읽음 유색표시(회색/연분홍)
 // @author       루냥이
-// @version      2025-02-27.rev7
+// @version      2025-02-27.rev8
 // @description  스텔라이브 네이버카페전용 방문페이지 및 공지사항 읽은 글 유색표시(회색/연분홍)
 // @match        https://*cafe.naver.com/ArticleList.nhn?search.clubid=29424353*
 // @match        https://*cafe.naver.com/ArticleRead.nhn*&clubid=29424353*
@@ -13,6 +13,6 @@
 // ==/UserScript==
 
 document.head.innerHTML+=`<style>a.tit:visited{color:#d3d3d3 !important;}</style>
-<style>#upperArticleList.article-board.m-tcol-c{tr.board-notice.type_required{a.article:visited{color:#FFD2D2 !important;}}}</style>
-<style>#upperArticleList.article-board.m-tcol-c{tr.board-notice.type_main{a.article:visited{color:#FFD2D2 !important;}}}</style>
-<style>#upperArticleList.article-board.m-tcol-c{tr.board-notice.type_up{a.article:visited{color:#d3d3d3 !important;}}}</style>`;
+<style>tr.board-notice.type_required{a.article:visited{color:#FFD2D2 !important;}}</style>
+<style>tr.board-notice.type_main{a.article:visited{color:#FFD2D2 !important;}}</style>
+<style>tr.board-notice.type_up{a.article:visited{color:#d3d3d3 !important;}}</style>`;
