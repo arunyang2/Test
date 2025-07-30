7a49b9471a173f82a2d54043aa837e1f265d608e
arunyang2 <txblade07@gmail.com>
2025-07-31 00:39:47 +0900

    Update Click_live_button_of_CHZZK.user.js

--- Click_live_button_of_CHZZK.user.js
+++ Click_live_button_of_CHZZK.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         통나무 파워 Auto Click
 // @author       루냥이
-// @version      2025-07-31 ver.002
+// @version      2025-07-31.rev003
 // @description  치지직 1시간 라이브 시청시 생기는 통나무 파워 버튼 자동으로 클릭하는 기능
 // @match        https://chzzk.naver.com/*
 // @icon         https://i.namu.wiki/i/Czd1U3HQ-Xi_DUp3KGvNHhdMo4EGJiSmoXiXXeJNGtWxCJ7BLIGXsAyGOnBf4Tr-L96HnlyuWMQsc52RJIe6HmGMJGbamtc8J4LfTMet3Zw7K4Mw2H8NZRGQcUVDDHL99SPdXtKwuUEL6xUJzshAtw.svg
